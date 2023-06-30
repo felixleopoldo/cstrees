575906d5cbd23b2a6d2a4f7b09430c160ac64863
Alex Markham <markham@kth.se>
2023-06-30 15:31:36 +0200

    can't quite figure out 2 cvars; changing to box terminology

--- src/cstrees/double_cvar_stagings.py
+++ src/cstrees/double_cvar_stagings.py
@@ -1,5 +1,6 @@
 """Enumerate CStrees and stagings with up to 2 context variables."""
-from itertools import combination, product, compress
+from itertools import combinations, product, compress
+from copy import deepcopy
 
 
 def num_stagings(lvl: int):
@@ -11,21 +12,41 @@ def max2_cvars_stagings(var_outcomes: list, possible_cvars: tuple = None):
     """Enumerate stagings at given level of CStree."""
     staging_no_context = [var_outcomes]
     yield staging_no_context
+
     degen = False
+
     num_vars = len(var_outcomes)
-    sub_possible_cvs = reversed(tuple(combination(num_vars, num_vars - 1)))
-    zipped = zip(sub_possible_cvs, one_cvar_stagings(var_outcomes, possible_cvars))
-    for possible_cvs, staging in zipped:
+    sub_possible_cvs = reversed(tuple(combinations(range(num_vars), num_vars - 1)))
+    z_stagings = zip(
+        sub_possible_cvs, one_cvar_stagings(var_outcomes, possible_cvars, num_vars)
+    )
+    for possible_cvs, staging in z_stagings:
         yield staging
-        zipped = zip(
-            one_cvar_stagings(staging[0], var), one_cvar_stagings(staging[1], var)
-        )  # need to replace var here with possible_cvs
-        for substaging_0, substaging_1 in zipped:
-            yield [staging[0]] + substaging_1
-            yield [staging[1]] + substaging_0
+
+        num_stages = len(staging)
+        for subset_size in range(1, num_stages):  # proper, nonempty subsets
+            subsets = combinations(range(subset_size), subset_size)
+            for subset in subsets:
+                substaging = [staging[i] for i in subset]
+                subposs_cvs = [possible_cvs[i] for i in subset]
+                substagings = list(one_cvar_stagings(substaging, subposs_cvs, num_vars))
+                print(f"\n{subset}")
+                yield [
+                    staging[i] if i in subset else substagings[i]
+                    for i in range(num_stages)
+                ]
+        # z_substagings = zip(
+        #     *(one_cvar_stagings(stage, possible_cvs, num_vars) for stage in staging)
+        # )
+        # for substagings in z_substagings:
+        #     for stage in staging:
+        #         for substaging in substagings:
+        #             yield [stage] + substaging
+
         prod = product(
-            one_cvar_stagings(staging[0], var), one_cvar_stagings(staging[1], var)
-        )
+            one_cvar_stagings(staging[0], possible_cvs, num_vars),
+            one_cvar_stagings(staging[1], possible_cvs, num_vars),
+        )  # need to replace var here with possible_cvs
         for substaging_0, substaging_1 in prod:
             if degen:
                 break
@@ -34,9 +55,8 @@ def max2_cvars_stagings(var_outcomes: list, possible_cvars: tuple = None):
                 degen = True
 
 
-def one_cvar_stagings(staging: list, possible_cvars: tuple):
+def one_cvar_stagings(staging: list, possible_cvars: tuple, num_vars: int):
     """Enumerate new stagings resulting from adding one cvar to given staging."""
-    num_vars = len(staging[0])
     pcv_mask = (var in possible_cvars for var in range(num_vars))
     for var, stage in zip(possible_cvars, compress(staging, pcv_mask)):
         yield [staging[:var] + [outcome] + staging[var + 1 :] for outcome in stage]
