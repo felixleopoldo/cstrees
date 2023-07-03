3c9e2b28541159c14658ee39939d182bc3231888
Alex Markham <markham@kth.se>
2023-07-03 17:06:40 +0200

    fixed bug in `codim_1_subdivs` list comp loop

--- src/cstrees/double_cvar_stagings.py
+++ src/cstrees/double_cvar_stagings.py
@@ -17,49 +17,73 @@ def codim_max2_boxes(box: list, splittable_dims: tuple = None):
     degen = False
 
     num_dims = len(box)
-    sub_splittable_dims = reversed(tuple(combinations(range(num_dims), num_dims - 1)))
+    if splittable_dims is None:
+        splittable_dims = range(num_dims)
+    sub_splittable_dims = reversed(tuple(combinations(splittable_dims, num_dims - 1)))
     z_cd1_subdivs = zip(
-        sub_splittable_dims, codim_1_subdivs(box, splittable_dims, num_dims)
+        sub_splittable_dims, codim_1_subdivs(codim_0_box, splittable_dims)
     )
     for poss_split_dims, cd1_subdiv in z_cd1_subdivs:
         yield cd1_subdiv
 
         num_cd1_boxes = len(cd1_subdiv)
+
         for subset_size in range(1, num_cd1_boxes):
             subsets = combinations(range(num_cd1_boxes), subset_size)
             for subset in subsets:
-                for fixed_dim, fixed_cd1_box in enumerate(cd1_box):
-                    to_split = [cd1_box[i] for i in sub_poss_split_dims]
-                # if len(sub_cd1_box) == 1:
-                #     sub_cd1_box = sub_cd1_box[0]
-                cd2_boxes = codim_1_subdivs(sub_cd1_box, sub_poss_split_dims, num_dims)
-                for cd2_box in cd2_boxes:
-                    mask = (i in sub_poss_split_dims for i in range(num_dims))
-                    zipped = zip_longest(mask, cd2_box, cd1_box)
-                    yield [
-                        sub_cd1_box if sub_mask else cd2_box
-                        for (sub_mask, cd2_box, sub_cd1_box) in zipped
-                    ]
+                for cd12_subdiv in codim_1_subdivs(cd1_subdiv, poss_split_dims, subset):
+                    yield cd12_subdiv
+                # for fixed_dim, fixed_cd1_box in enumerate(cd1_box):
+                #     to_split = [cd1_box[i] for i in sub_poss_split_dims]
+                # # if len(sub_cd1_box) == 1:
+                # #     sub_cd1_box = sub_cd1_box[0]
+                # cd2_boxes = codim_1_subdivs(sub_cd1_box, sub_poss_split_dims)
+                # for cd2_box in cd2_boxes:
+                #     mask = (i in sub_poss_split_dims for i in range(num_dims))
+                #     zipped = zip_longest(mask, cd2_box, cd1_box)
+                #     yield [
+                #         sub_cd1_box if sub_mask else cd2_box
+                #         for (sub_mask, cd2_box, sub_cd1_box) in zipped
+                #     ]
+        if degen:
+            break
+        for cd2_subdiv in codim_1_subdivs(cd1_subdiv, poss_split_dims):
+            yield cd2_subdiv
+        if len(box) == 2:
+            degen = True
 
-        prod = product(
-            codim_1_subdivs(cd1_box[0], poss_split_dims, num_dims),
-            codim_1_subdivs(cd1_box[1], poss_split_dims, num_dims),
-        )  # need to replace var here with possible_cvs
-        for substaging_0, substaging_1 in prod:
-            if degen:
-                break
-            yield substaging_0 + substaging_1
-            if len(box) == 2:
-                degen = True
+        # prod = product(
+        #     codim_1_subdivs(cd1_box[0], poss_split_dims),
+        #     codim_1_subdivs(cd1_box[1], poss_split_dims),
+        # )  # need to replace var here with possible_cvs
+        # for substaging_0, substaging_1 in prod:
+        #     if degen:
+        #         break
+        #     yield substaging_0 + substaging_1
+        #     if len(box) == 2:
+        #         degen = True
 
 
-def codim_1_subdivs(box: list, splittable_dims: tuple):
+def codim_1_subdivs(
+    box: list, splittable_dims: tuple, splittable_subboxes: tuple = None
+):
     """Enumerate codimension-1 subdivisions of the given (subdivision of a) box."""
-    num_dims = len(box[0])
+    if splittable_subboxes is None:
+        splittable_subboxes = range(len(box))
     for dim in splittable_dims:
         points = box[0][dim]
-        yield [
-            subbox[:dim] + [point] + subbox[dim + 1 :]
-            for point in points
-            for subbox in box
-        ]
+        # yield [
+        #     subbox[:dim] + [point] + subbox[dim + 1 :]
+        #     if subbox_idx in splittable_subboxes
+        #     else subbox
+        #     for subbox_idx, subbox in enumerate(box)
+        #     for point in points
+        # ]
+        cd1_subdiv = []
+        for subbox_idx, subbox in enumerate(box):
+            if subbox_idx in splittable_subboxes:
+                for point in points:
+                    cd1_subdiv += [subbox[:dim] + [point] + subbox[dim + 1 :]]
+            else:
+                cd1_subdiv += [subbox]
+        yield cd1_subdiv
