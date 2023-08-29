1dd48e13725b3d1fd1fd6dd2232dc8f3d8ca998c
Felix Rios <felix.rios@rocafuerte.se>
2023-08-29 13:54:56 +0200

    docs..

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ sys.path.append(os.path.abspath('../src/cstrees'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '5.0'
+needs_sphinx = '4.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
