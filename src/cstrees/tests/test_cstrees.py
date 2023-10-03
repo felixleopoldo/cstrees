f6a185dd7d448b10ad064df88d86188cd63f63fb
Alex Markham <markham@kth.se>
2023-10-03 12:53:14 -0500

    more readable test

--- src/cstrees/tests/test_cstrees.py
+++ src/cstrees/tests/test_cstrees.py
@@ -24,21 +24,21 @@ def test_predict():
     t.sample(100)
 
     # test empty
-    empty_outcome = {}
-    assert t.predict(empty_outcome) == (2, 0, 1, 1)
-    assert t.predict(empty_outcome, True) == ((2, 0, 1, 1), 0.11590809641032397)
+    empty_observation = {}
+    assert t.predict(empty_observation) == (2, 0, 1, 1)
+    assert t.predict(empty_observation, True) == ((2, 0, 1, 1), 0.11590809641032397)
 
     # test complete
-    complete_outcome = {idx: outcome for idx, outcome in enumerate((2, 0, 1, 1))}
-    assert t.predict(complete_outcome, True) == ((2, 0, 1, 1), 1.0)
+    complete_observation = {0: 2, 1: 0, 2: 1, 3: 1}
+    assert t.predict(complete_observation, True) == ((2, 0, 1, 1), 1.0)
 
     # partials
-    partial_outcome_1 = {0: 1}
-    t.predict(partial_outcome_1, True)
-    assert t.predict(partial_outcome_1, True) == ((1, 0, 1, 1), 0.26674411494959)
+    partial_observation_1 = {0: 1}
+    t.predict(partial_observation_1, True)
+    assert t.predict(partial_observation_1, True) == ((1, 0, 1, 1), 0.26674411494959)
 
-    partial_outcome_2 = {0: 1, 3: 2}
-    assert t.predict(partial_outcome_2, True) == ((1, 1, 1, 2), 0.3829026812192928)
+    partial_observation_2 = {0: 1, 3: 2}
+    assert t.predict(partial_observation_2, True) == ((1, 1, 1, 2), 0.3829026812192928)
 
-    partial_outcome_3 = {0: 1, 2: 0, 3: 2}
-    assert t.predict(partial_outcome_3, True) == ((1, 0, 0, 2), 0.5913854582044948)
+    partial_observation_3 = {0: 1, 2: 0, 3: 2}
+    assert t.predict(partial_observation_3, True) == ((1, 0, 0, 2), 0.5913854582044948)
