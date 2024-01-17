06589869ad022824a3202b1d5b1cc4fe45f648e9
Alex Markham <alex.markham@causal.dev>
2024-01-17 22:12:49 +0100

    skeleton for KL divergence module

---
+++ src/cstrees/evaluate.py
@@ -0,0 +1,13 @@
+"""Evaluate estimated CStrees."""
+import cstrees
+
+
+def kl_divergence(estimated: CStree, true: CStree) -> float:
+    """Quantify how distribution of estimated CStree differs from that of true CStree.
+
+    Notes:
+    See the `KL divergence
+    <https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence>`_.
+    """
+    divergence = 0
+    return divergence
